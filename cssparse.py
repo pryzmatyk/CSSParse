f9f91b5f9a020b3ebaf01b2145e9445fbb5d2205
tclausen <tclausen@paypal.com>
2020-06-30 11:10:51 -0700

    increment version to v1, add comment parsing, add sub parsing for nested braces, add element ids for differentiation

--- cssparse.py
+++ cssparse.py
@@ -1,23 +1,18 @@
-import re
+'''CssParse - A Simple CSS Parser'''
+
+__version__ = "1.0.0"
 
 class CssParse:
     """A simple CSS style parser.
 
-   It is a fast way to search for selectors and styles.
+    It is a fast way to search for selectors and styles.
     """
 
     def __init__(self):
-        self.buffer = []
-        self.output = dict()
-        self.css = str()
+        pass
 
-    def __str__(self):
-        return str(self.output)
-
-    def __repr__(self):
-        return str(self.output)
-
-    def __strip(self, css) -> str:
+    @staticmethod
+    def _strip(css) -> str:
         """Strip the CSS string.
 
         Ensuring cleaning of any extra whitespace.
@@ -28,11 +23,32 @@ class CssParse:
         Returns:
             str -- Stripped CSS string.
         """
-        assert isinstance(css, str), False
-        if isinstance(css, str):
-            stripped = re.sub(r"[\n\r\t]", "", css)
-            stripped = re.sub(r"[\\]{1,6}[nrt]", "", stripped)
-            return str(stripped)
+        return ''.join(list([char for char in css if char not in list(["\n", "\r", "\t"])]))
+
+    def _format(self, buffer):
+        reparsed_buffer = dict()
+        formatted_buffer = list()
+        output_selector = list()
+        if any(brace in buffer for brace in ["{", "}"]):
+            reparsed_buffer = self.parse(''.join(buffer))
+        else:
+            if ";" in buffer:
+                formatted_buffer = list(filter(None, "".join(buffer).replace(": ", ":").split(";")))
+                output_selector = [value.strip(" ") for value in formatted_buffer if value is not None and value != "" and value != " "]
+            else:
+                formatted_buffer = "".join(buffer).replace(": ", ":")
+                output_selector = list([formatted_buffer])
+            output_selector_dict = list()
+            for selector in output_selector:
+                selector_split = selector.split(":")
+                if len(selector_split) == 2:
+                    output_selector_dict.append({selector_split[0]: selector_split[1]})
+                else:
+                    output_selector_dict.append(selector)
+        if reparsed_buffer:
+            return reparsed_buffer
+        else:
+            return output_selector_dict
 
     def parse(self, css) -> dict:
         """Parse the CSS string into a dict of lists.
@@ -44,28 +60,62 @@ class CssParse:
             css {str} -- String of CSS data from external style sheet.
 
         Returns:
-            self.output dict -- Dict of selector names and styles
+            output dict -- Dict of selector names and styles
 
             Example:
                 margin-bottom:60px;display:block
         """
         assert isinstance(css, str), False
-        if not isinstance(css, str):
-            return []
-        else:
-            self.css = self.__strip(css)
-            b = 0
+        buffer = list()
+        comment = list()
+        output = dict()
+        if isinstance(css, str):
+            css = self._strip(css)
+            brace_depth = 0
             key_name = str()
-            for _ in enumerate(self.css):
-                if "{" in self.css[b]:
-                    key_name = str(''.join(self.buffer))
-                    self.output[key_name] = str()
-                    self.buffer = []
-                elif "}" in self.css[b] and self.buffer:
-                    self.output[key_name] = set(filter(None, "".join(self.buffer).replace(": ", ":").split(";")))
-                    self.output[key_name] = [value.strip(" ") for value in self.output[key_name] if value is not None and value != "" and value != " "]
-                    self.buffer = []
+            key_name_comment = str()
+            for char_pos, _ in enumerate(css):
+                if "/*" in css[char_pos:char_pos+2]:
+                    key_name_comment = f"comment ___{char_pos}"
+                    output[key_name_comment] = str()
+                    comment = list()
+                    comment.append(css[char_pos])
+                elif "*/" in css[char_pos-2:char_pos]:
+                    comment.append(css[char_pos])
+                    output[key_name_comment] = ''.join(comment)
+                    comment = list()
+                elif comment:
+                    comment.append(css[char_pos])
                 else:
-                    self.buffer.append(self.css[b])
-                b += 1
-            return self.output
+                    if "{" in css[char_pos] and brace_depth == 0:
+                        brace_depth += 1
+                        temp_suffix = " ___{}".format(char_pos)
+                        key_name = str(''.join(buffer)) + temp_suffix
+                        output[key_name] = str()
+                        buffer = list()
+                        continue
+                    else:
+                        buffer.append(css[char_pos])
+
+                    if "}" in css[char_pos] and brace_depth > 0:
+                        brace_depth -= 1
+                    elif "{" in css[char_pos] and brace_depth > 0:
+                        brace_depth += 1
+
+                    if "}" in css[char_pos] and buffer and brace_depth <= 0:
+                        buffer.pop()
+                        output[key_name] = self._format(buffer)
+                        buffer = list()
+                        brace_depth = 0
+        return output
+
+def main():
+    import json
+    css_parser = CssParse()
+    parsed_css_file = css_parser.parse(open("git.css").read())
+    json_dump = json.dumps(parsed_css_file)
+    with open("testparsed.json", "w") as file:
+        file.write(json_dump)
+
+if __name__ == "__main__":
+    main()
